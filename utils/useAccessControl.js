f587c1aa909eaf8cfd647bf7b3f944575eadce33
Zeshan124 <raeeszeeshan4@gmail.com>
2025-07-23 16:36:18 +0500

    Updated site

--- utils/useAccessControl.js
+++ utils/useAccessControl.js
@@ -1,13 +1,12 @@
 // hooks/useAccessControl.js
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import accessRights from '~/utils/rolesConfig'; // Adjust the import path as necessary
-import Cookies from 'js-cookie';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import accessRights from "~/utils/rolesConfig"; // Adjust the import path as necessary
+import Cookies from "js-cookie";
 
 const pathMatches = (path, pattern) => {
-
-  const escapedPattern = pattern.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&');
-  const paramRegex = escapedPattern.replace(/\\\[(.*?)\\\]/g, '([^\/]+)');
+  const escapedPattern = pattern.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&");
+  const paramRegex = escapedPattern.replace(/\\\[(.*?)\\\]/g, "([^\/]+)");
   const regex = new RegExp(`^${paramRegex}$`);
   return regex.test(path);
 };
@@ -16,29 +15,26 @@ const useAccessControl = () => {
   const router = useRouter();
 
   useEffect(() => {
-
     const path = router.pathname;
 
-    if(!path.startsWith('/login')){
-      const role = Cookies.get('roleID');
+    if (!path.startsWith("/login")) {
+      const role = Cookies.get("roleID");
       const roleConfig = accessRights[role];
 
       const isAllowedToAccess = roleConfig?.allowedPages.some((allowedPage) =>
         pathMatches(path, allowedPage)
       );
 
-
-      if(Cookies.get('token')){
+      if (Cookies.get("token")) {
         if (!roleConfig || !isAllowedToAccess) {
           // Redirect to a 403 page if the user does not have access to the path
-          router.push('/403'); // or any other fallback page
+          router.push("/403"); // or any other fallback page
         }
-      }else{
-        router.push('/login')
+      } else {
+        router.push("/login");
       }
-  }
-
-  }, [router.pathname ]);
+    }
+  }, [router.pathname]);
 };
 
 export default useAccessControl;
